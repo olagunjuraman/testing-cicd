d93caac4654c0785aafab01bf5d9379b756a31cb
raman <raman@figo.africa>
2024-01-19 17:02:45 +0100

    testing

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 6000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, Kubernetes with Traefik!");
+  res.send("Hello, Kubernetes with Traefik Testing!");
 });
 
 app.listen(PORT, () => {
